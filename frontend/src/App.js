cbf52acb51c7702809b21ea6f3438ae176da1ba2
priyanshu5656 <priyanshukumar906@gmail.com>
2023-04-23 22:25:11 +0530

    changed hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,36 +15,7 @@ import Partner from './components/Partner'
 
 
 function App(){
-  const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-      getProducts();
-  }, [])
-
-const getProducts = async () => {
-  let result = await fetch('http://localhost:5000/priyanshu',{
-  mode:"cors"
-  });
-  result = await result.json();
-  setProducts(result);
-}
-console.log(products);
-const [pg, setPgs] = useState(products);
-  function removePgs(name) {
-    const newPgs = pg.filter(pg => pg.name !== name);
-    setPgs(newPgs);
-  }
-
-  if(pg.length === 0) {
-    return (
-        <div className="refresh">
-          <h2>No Pgs Left</h2>
-          <button className="btn-white" onClick={() => setPgs(products)}>
-            Refresh
-          </button>
-        </div>
-    );
-  }
 
     return(
        <>
